ee9a9a1ad0acfda84dff785320da17b6ea0ddc0f
joeoakes <jxo19@psu.edu>
2023-11-06 23:45:59 -0500

    commit

---
+++ MapExample.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+    public static void main(String[] args) {
+        // Create a new HashMap
+        Map<String, Integer> userAges = new HashMap<>();
+
+        // Add key-value pairs to the HashMap
+        userAges.put("Alice", 28);
+        userAges.put("Bob", 24);
+        userAges.put("Charlie", 33);
+
+        // Retrieve the age of 'Bob'
+        System.out.println("Bob's Age: " + userAges.get("Bob"));
+
+        // Iterate over the entries in the map
+        for (Map.Entry<String, Integer> entry : userAges.entrySet()) {
+            String user = entry.getKey();
+            Integer age = entry.getValue();
+            System.out.println(user + " is " + age + " years old.");
+        }
+
+        // Check if a key exists in the map
+        if (userAges.containsKey("Alice")) {
+            System.out.println("Alice is in the map.");
+        }
+
+        // Check if a value exists in the map
+        if (userAges.containsValue(33)) {
+            System.out.println("There is a user who is 33 years old.");
+        }
+
+        // Remove a key-value pair
+        userAges.remove("Charlie");
+        System.out.println("Charlie has been removed from the map.");
+
+        // Replace a value associated with a key
+        userAges.put("Alice", 29);
+        System.out.println("Alice's new age is " + userAges.get("Alice"));
+
+        // Find the size of the map
+        int size = userAges.size();
+        System.out.println("The map contains " + size + " entries.");
+    }
+}
+
